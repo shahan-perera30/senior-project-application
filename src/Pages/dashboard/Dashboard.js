974b75eab17f65e60a735ad486f19a284fbc036b
shahanperera <pererashahan264@gmail.com>
2023-11-10 05:14:25 -0600

    Dashboard styling

--- src/Pages/dashboard/Dashboard.js
+++ src/Pages/dashboard/Dashboard.js
@@ -19,33 +19,6 @@ function Dashboard() {
             <h1>Welcome back Shahan!</h1>
             <h2>Employee Position Title</h2>
             <p>Obsolescence Solved</p>
-            <div className="clock">
-              <a href="#" className="button1">
-                Clock in
-              </a>
-              <p>
-                Elapsed Time <span>2h 50m 45s</span>
-              </p>
-            </div>
-          </div>
-        </div>
-
-        <div className="cards">
-          <div className="filters">
-            <div className="recent">
-              <h2>Tasks</h2>
-              <a href="" className="button2">
-                Recent
-              </a>
-            </div>
-            <div className="filter_buttons">
-              <a href="" className="button1">
-                Filter
-              </a>
-              <a href="" className="button2">
-                Clock in
-              </a>
-            </div>
           </div>
         </div>
       </div>
