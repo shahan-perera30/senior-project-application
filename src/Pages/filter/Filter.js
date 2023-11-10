091cd578144ffe8037bf280adafd8b7b9b5f29d4
shahanperera <pererashahan264@gmail.com>
2023-11-10 16:57:36 -0600

    Filter button import csv

--- src/Pages/filter/Filter.js
+++ src/Pages/filter/Filter.js
@@ -1,66 +1,88 @@
 import React, { useState } from "react";
 import "./filter.css";
-import { csv } from "d3";
 
 function Filter() {
-  const [selectedFile, setSelectedFile] = useState(null);
-  const [columnsToKeep, setColumnsToKeep] = useState([]);
-  const [newCSVFile, setNewCSVFile] = useState(null);
+  const [file, setFile] = useState();
+  const [array, setArray] = useState([]);
 
-  const handleFileUpload = async (event) => {
-    const file = event.target.files[0];
-    setSelectedFile(file);
+  const fileReader = new FileReader();
 
-    const data = await csv(file).then((data) => {
-      const columns = Object.keys(data[0]);
-      setColumnsToKeep(columns);
-      return data;
+  const handleOnChange = (e) => {
+    setFile(e.target.files[0]);
+  };
+
+  const csvFileToArray = (string) => {
+    const csvHeader = string.slice(0, string.indexOf("\n")).split(",");
+    const csvRows = string.slice(string.indexOf("\n") + 1).split("\n");
+
+    const array = csvRows.map((i) => {
+      const values = i.split(",");
+      const obj = csvHeader.reduce((object, header, index) => {
+        object[header] = values[index];
+        return object;
+      }, {});
+      return obj;
     });
+
+    setArray(array);
   };
 
-  const filterDataAndCreateNewFile = async () => {
-    if (!selectedFile || columnsToKeep.length === 0) {
-      return;
+  const handleOnSubmit = (e) => {
+    e.preventDefault();
+
+    if (file) {
+      fileReader.onload = function (event) {
+        const text = event.target.result;
+        csvFileToArray(text);
+      };
+
+      fileReader.readAsText(file);
     }
-
-    const reader = new FileReader();
-    reader.onload = async (event) => {
-      const data = await csv(event.target.result).then((data) => {
-        return data.map((row) => {
-          const newRow = {};
-          columnsToKeep.forEach((column) => {
-            newRow[column] = row[column];
-          });
-          return newRow;
-        });
-      });
-
-      const newCSVFile =
-        "data:text/csv;charset=utf-8," +
-        data
-          .map((row) => columnsToKeep.map((column) => row[column]).join(","))
-          .join("\n");
-      setNewCSVFile(newCSVFile);
-    };
-
-    reader.readAsText(selectedFile);
   };
+
+  const headerKeys = Object.keys(Object.assign({}, ...array));
 
   return (
-    <div className="Filter">
-      <h1>Filter</h1>
-      <div className="fileUploadContainer">
-        <label htmlFor="fileUpload">Upload here</label>
-        <input id="fileUpload" type="file" onChange={handleFileUpload} />
-      </div>
-      <button onClick={filterDataAndCreateNewFile}>
-        Filter Data and Create New CSV
-      </button>
-      {newCSVFile && (
-        <a href={newCSVFile} download="filtered.csv">
-          Download New File
-        </a>
-      )}
+    <div style={{ textAlign: "center" }}>
+      <h1>REACTJS CSV IMPORT</h1>
+      <form>
+        <input
+          type={"file"}
+          id={"csvFileInput"}
+          accept={".csv"}
+          onChange={handleOnChange}
+        />
+
+        <button
+          onClick={(e) => {
+            handleOnSubmit(e);
+          }}
+        >
+          IMPORT CSV
+        </button>
+      </form>
+
+      <br />
+
+      <table>
+        <thead>
+          <tr key={"header"}>
+            {headerKeys.map((key) => (
+              <th>{key}</th>
+            ))}
+          </tr>
+        </thead>
+
+        <tbody>
+          {array.map((item) => (
+            <tr key={item.id}>
+              {Object.values(item).map((val) => (
+                <td>{val}</td>
+              ))}
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
