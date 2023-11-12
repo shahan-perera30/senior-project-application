b09c8726c81a8454f6662e9e292fe70064abaedc
shahanperera <pererashahan264@gmail.com>
2023-11-12 12:19:12 -0600

    Home Page Styling

--- src/Pages/home/Home.js
+++ src/Pages/home/Home.js
@@ -1,5 +1,10 @@
 import React from "react";
+import "/Users/shahanperera/ReactProjects/senior-project-application/src/Pages/home/Home.css";
 
 export default function Home() {
-  return <h1>Home</h1>;
+  return (
+    <div className="Home">
+      <h1>Welcome to the Home Page</h1>
+    </div>
+  );
 }
