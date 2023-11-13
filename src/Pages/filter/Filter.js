a0f3295ce0759ead4b236e206c417d672e8718f5
shahanperera <pererashahan264@gmail.com>
2023-11-13 00:15:28 -0600

    Database Filtering

--- src/Pages/filter/Filter.js
+++ src/Pages/filter/Filter.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import "./filter.css";
 import csv from "csv-parser";
+import fs from "fs";
 
 function Filter() {
   const [file, setFile] = useState();
@@ -12,21 +13,25 @@ function Filter() {
     setFile(e.target.files[0]);
   };
 
-  const fs = require("fs");
-
   function writeToFile(array, filename) {
-    fs.writeFile("./extracted.csv", array.join("/n"), (err) => {
-      if (err) throw err;
-      console.log("The file has been saved.");
-    });
+    var csvData = array.join("\n");
+    var blob = new Blob([csvData], { type: "text/csv;charset=utf-8;" });
+    var link = document.createElement("a");
+    var url = URL.createObjectURL(blob);
+    link.setAttribute("href", url);
+    link.setAttribute("download", filename);
+    link.style.visibility = "hidden";
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
   }
 
   const csvFileToArray = (string) => {
     const csvHeader = string.slice(0, string.indexOf("\n")).split(",");
     const csvRows = string.slice(string.indexOf("\n") + 1).split("\n");
 
-    const array = csvRows.map((i) => {
-      const values = i.split(",");
+    const array = csvRows.map((row) => {
+      const values = row.split(",");
       const obj = csvHeader.reduce((object, header, index) => {
         object[header] = values[index];
         return object;
@@ -55,15 +60,43 @@ function Filter() {
 
     reader.onload = (e) => {
       const csvText = e.target.result;
+      const csvRows = csvText.split("\n");
+      const csvHeader = csvRows.shift().split(",");
       const results = [];
 
       csvText.split("\n").forEach((row) => {
         const columns = row.split(",");
         // Change the index to the column you want (0-based)
-        const specificColumn = columns[2]; // Replace '2' with the desired column index
+        const specificColumn = columns[1]; // Replace '2' with the desired column index
         results.push(specificColumn);
-        const specificColumn2 = columns[4]; // Replace '2' with the desired column index
+        const specificColumn2 = columns[9]; // Replace '2' with the desired column index
         results.push(specificColumn2);
+        const specificColumn3 = columns[11]; // Replace '2' with the desired column index
+        results.push(specificColumn3);
+        const specificColumn4 = columns[18]; // Replace '2' with the desired column index
+        results.push(specificColumn4);
+        const specificColumn5 = columns[20]; // Replace '2' with the desired column index
+        results.push(specificColumn5);
+        const specificColumn6 = columns[22]; // Replace '2' with the desired column index
+        results.push(specificColumn6);
+        const specificColumn7 = columns[24]; // Replace '2' with the desired column index
+        results.push(specificColumn7);
+        const specificColumn8 = columns[26]; // Replace '2' with the desired column index
+        results.push(specificColumn8);
+        const specificColumn9 = columns[28]; // Replace '2' with the desired column index
+        results.push(specificColumn9);
+        const specificColumn10 = columns[33]; // Replace '2' with the desired column index
+        results.push(specificColumn10);
+        const specificColumn11 = columns[35]; // Replace '2' with the desired column index
+        results.push(specificColumn11);
+        const specificColumn12 = columns[65]; // Replace '2' with the desired column index
+        results.push(specificColumn12);
+        const specificColumn13 = columns[64]; // Replace '2' with the desired column index
+        results.push(specificColumn13);
+        const specificColumn14 = columns[66]; // Replace '2' with the desired column index
+        results.push(specificColumn14);
+        const specificColumn15 = columns[67]; // Replace '2' with the desired column index
+        results.push(specificColumn15);
       });
 
       console.log(results);
